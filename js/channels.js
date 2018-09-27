98c7acfd010e1cb8e7db45820b45d8e4fae833e6
Isidoro <isidoro@posteo.net>
2018-09-27 21:22:41 +0200

    channels list

---
+++ js/channels.js
@@ -0,0 +1,37 @@
+// #7.2 File to store channels
+
+// global variable yummy
+var yummy = {name: '#Yummy',
+             createdOn: Date(2016, 4,1),
+             createdBy: 'minus.plus.yummy',
+             starred: false,
+             expiresIn: 100*60*1000,
+             messageCount: 999}
+
+var sevenContinents = {name: '#SevenContinents',
+                       createdOn: Date(2016, 4,1),
+                       createdBy: 'minus.plus.yummy',
+                       starred: true,
+                       expiresIn: 100*60*1000,
+                       messageCount: 999}
+
+var killerApp = {name: '#KillerApp',
+                 createdOn: Date(2016, 4,1),
+                 createdBy: 'minus.plus.yummy',
+                 starred: false,
+                 expiresIn: 100*60*1000,
+                 messageCount: 999}
+
+var firstPersonOnMars = {name: '#FirstPersonOnMars',
+                         createdOn: Date(2016, 4,1),
+                         createdBy: 'minus.plus.yummy',
+                         starred: true,
+                         expiresIn: 100*60*1000,
+                         messageCount: 999}
+
+var octoberfest = {name: '#Octoberfest',
+                   createdOn: Date(2016, 4,1),
+                   createdBy: 'minus.plus.yummy',
+                   starred: false,
+                   expiresIn: 100*60*1000,
+                   messageCount: 999}
